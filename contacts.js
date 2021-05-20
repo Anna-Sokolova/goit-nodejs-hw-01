6023e899e73585d993490580270b14bf697c81f2
Anna Sokolova <sokolovaskorik@gmail.com>
2021-05-21 00:25:37 +0200

    Added all functions

--- contacts.js
+++ contacts.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs").promises;
-const colors = require('colors');
+require("colors");
+const { nanoid } = require("nanoid");
 
 //Создай переменную contactsPath и запиши в нее путь к файлу contacts.json
 const contactsPath = path.resolve("db/contacts.json");
@@ -8,48 +9,72 @@ const contactsPath = path.resolve("db/contacts.json");
 //получаем массив всех контактов
 async function listContacts() {
   try {
-    const response = await fs.readFile(contactsPath);
+    const response = await fs.readFile(contactsPath, "utf8");
     const data = JSON.parse(response);
-    console.log(data);
-    return data;
+    const currentContacts = data;
+    console.table(currentContacts);
+    return currentContacts;
   } catch (error) {
     console.error(error.message);
   }
 }
 
-listContacts();
-
 //ищем контакт по id
 async function getContactById(contactId) {
   try {
-    const response = await fs.readFile(contactsPath);
+    const response = await fs.readFile(contactsPath, "utf8");
     const data = JSON.parse(response);
-    const findContactById = data.find((contact) => contactId === contact.id);
-    console.log(findContactById);
+    const currentContacts = data;
+    const findContactById = currentContacts.find((contact) => contactId === contact.id);
+    if (!findContactById) {
+      console.log(`Такого контакта c id ${contactId} не существует`.bgRed);
+      return;
+    }
+    console.log(`Контакт с id ${contactId} найден`.bgYellow);
+    console.table(findContactById);
     return findContactById;
   } catch (error) {
     console.error(error.message);
   }
 }
 
-getContactById(5);
-
 //удаляем контакт
 async function removeContact(contactId) {
   try {
-    const response = await fs.readFile(contactsPath);
+    const response = await fs.readFile(contactsPath, "utf8");
     const data = JSON.parse(response);
-    const removeContactById = data.filter((contact) => contactId !== contact.id);
-    console.log(removeContactById);
-    // const newData =
-    return removeContactById;
+    const currentContacts = data;
+    const updateContacts = currentContacts.filter((contact) => contactId !== contact.id);
+    console.log(`Контакт с id ${contactId} удален`.bgCyan);
+    console.table(updateContacts);
+
+    await fs.writeFile(contactsPath, JSON.stringify(updateContacts), "utf8");
+
+    return updateContacts;
   } catch (error) {
     console.error(error.message);
   }
 }
 
-removeContact(2)
+//добавляем контакт
+async function addContact(name, email, phone) {
+  try {
+    const response = await fs.readFile(contactsPath, "utf8");
+    const data = JSON.parse(response);
+    const currentContacts = data;
 
-// function addContact(name, email, phone) {
-//   // ...твой код
-// }
+    const id = nanoid(10);
+    const newContact = { id, name, email, phone };
+    const newContacts = [...currentContacts, newContact];
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts), "utf8");
+
+    console.log(` Новый контакт ${name} добавлен в таблицу `.bgMagenta);
+    console.table(newContacts);
+
+    return newContacts;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
